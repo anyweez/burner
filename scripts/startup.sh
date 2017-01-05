900d100cce7c8d4bc893da2c1616c52b5073c3e4
Luke Segars <luke@lukesegars.com>
2017-01-04 22:21:42 -0800

    Removed misplaced commands from scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,3 @@
-
-# Packages should automatically update
-sudo apt-get install unattended-upgrades 
-echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
+# Update the system after first install
+sudo apt-get update
+sudo apt-get upgrade
